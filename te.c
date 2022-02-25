a8c443d9d402cebc5d5c47b0ca3a417a784d7bb8
Othman Mallah <othmanmallah15@gmail.com>
2022-02-25 15:19:02 +0100

    poo

---
+++ te.c
@@ -0,0 +1,58 @@
+void	ft_print_parametre(int s, char **b)
+{
+	int	i;
+	int	a;
+
+	i = 0;
+	a = 1;
+	while (a <= (s - 1))
+	{
+		while (b[a][i])
+		{
+			write (1, &b[a][i], 1);
+			i++;
+		}
+		i = 0;
+		write (1, "\n", 1);
+		a++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		k;
+	char	*temp;
+
+	i = 1;
+	if (ac > 1)
+	{
+		while (i < (ac - 1))
+		{
+			k = ft_strcmp(av[i], av[i + 1]);
+			if (k > 0)
+			{
+				temp = av[i];
+				av[i] = av[i + 1];
+				av[i + 1] = temp;
+				i = 0;
+			}
+			i++;
+		}
+		ft_print_parametre(ac, av);
+	}
+}
--- test.c
+++ test.c
@@ -11,6 +11,14 @@ int	checker(char *str)
 {
 	int num;
 
+	if (str[0] == '0' || (str[0] == '-' && str[1] == '1'))
+	{
+		num = ft_atoi(str);
+		if (num >= -2147483648 && num <= 2147483647)
+			return (1);
+		else
+			return (5);
+	}
 	num = ft_atoi(str);
 	if (num != 0 && num != -1)
 	{
@@ -24,40 +32,64 @@ int	checker(char *str)
 	return 0;
 }
 
+void	only_num(char *str)
+{
+	int i = 0;
+	int res;
+
+	if (str[0] == '-' || str[0] == '+')
+		i = 1;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		else
+		{
+			write (1, "only num\n", 10);
+			exit(0);
+		}
+	}
+	res = checker(str);
+	if (res != 1)
+	{
+		write (1, "only integer\n", 14);
+		exit(0);
+	}
+}
+
+void	ft_print_parametre(int s, char **b)
+{
+	int	i;
+	int	a;
+
+	i = 0;
+	a = 1;
+	while (a <= (s - 1))
+	{
+		while (b[a][i])
+		{
+			write (1, &b[a][i], 1);
+			i++;
+		}
+		i = 0;
+		write (1, "\n", 1);
+		a++;
+	}
+}
 
 int main(int ac, char **av)
 {
-	int i;
-	int j;
-	int	res;
+	int j, i;
 	int num;
-	t_test id;
+	char	*temp;
 
-	i = 0;
 	j = 1;
+	i = 1;
 	if (ac >= 3)
 	{
 		while (av[j])
 		{
-			if (av[j][0] == '-' || av[j][0] == '+')
-				i = 1;
-			while (av[j][i])
-			{
-				if (av[j][i] >= '0' && av[j][i] <= '9')
-					i++;
-				else
-				{
-					write (1, "only num\n", 10);
-					exit(0);
-				}
-			}
-			res = checker(av[j]);
-			if (res != 1)
-			{
-				write (1, "only integer\n", 14);
-				exit(0);
-			}
-			i = 0;
+			only_num(av[j]);
 			j++;
 		}
 		j = 1;
@@ -80,6 +112,21 @@ int main(int ac, char **av)
 			i = 1;
 			j++;
 		}
+		j = 1;
+		while(j < (ac - 1))
+		{
+			num = strcmp(av[j], av[j + 1]);
+			if (num > 0)
+			{
+				temp = av[j];
+				av[j] = av[j + 1];
+				av[j + 1] = temp;
+				j = -1;
+			}
+			j++;
+		} 
+		ft_print_parametre(ac, av);
+
 	}
 	else
 		write (1, "moore", 6);
